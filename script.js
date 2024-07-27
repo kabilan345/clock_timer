a07c6ea31a1e36517c92fb51173b8a8f96ea7bc8
kabilan <kabilan.b2003@gmail.com>
2024-07-27 21:50:32 +0530

    timer

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let seconds = 0;
 function formatTime(sec) {                         //formatTime(3661)
     let hrs = Math.floor(sec / 3600);              //Math.floor(3661 / 3600) → 1 hour
     let mins = Math.floor((sec % 3600) / 60);     //Math.floor((3661 % 3600) / 60) → Math.floor(61 / 60) → 1 minute
-    let secs = sec % 60;                          //3661 % 60 → 1 second.
+    let secs = sec % 60;                          //3661 % 60 → 1 second
 
     return `${String(hrs).padStart(2, '0')}:${String(mins).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;  //function formatTime(3661) returns the string "01:01:01".
 }
